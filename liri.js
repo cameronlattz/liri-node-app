2f09006f6e784129262e610f4f689a9bdd151284
Cameron Lattz <camerolattz@gmail.com>
2019-09-10 16:59:45 -0500

    finished functionality, still need to restructure

--- liri.js
+++ liri.js
@@ -1,100 +1,149 @@
-require("dotenv").config();
-const keys = require("./keys.js");
 const [,,command,...args] = process.argv;
 const value = args.length > 0 ? args.join(" ") : void 0;
 
-const liri = (function(command, value) {
+(function(command, value) {
+    require("dotenv").config();
+    const keys = require("./keys.js");
     const _fs = require("fs");
+    const _axios = require("axios");
     const Spotify = require("node-spotify-api");
     var _spotify = new Spotify(keys.spotify);
-    const _axios = require("axios");
 
-    const _output = function(message, showInTerminal=true) {
-        if (showInTerminal) {
-            console.log(message);
+    const _commandSwitch = function(command, value) {
+        switch (command) {
+            case "concert-this":
+                if (value != void 0) {
+                    _concert_this(value);
+                } else {
+                    _output("", "Please enter an artist name.", true, true);
+                    _outputEnd();
+                }
+                break;
+            case "spotify-this-song":
+                // do this better
+                if (value != void 0) {
+                    _spotify_this_song(value);
+                } else {
+                    _spotify_this_song();
+                }
+                break;
+            case "movie-this":
+                // do this better
+                if (value != void 0) {
+                    _movie_this(value);
+                } else {
+                    _movie_this();
+                }
+                break;
+            case "do-what-it-says":
+                _do_what_it_says();
+                break;
+            default:
+                _output("", "Please input a valid command.", true, true);
+                _outputEnd();
+                break;
         }
-        _fs.appendFile("log.txt", message + "\n", function(error) {
-            if (error) return console.log(error);
-        });
     }
     
-    const _concert = function(value) {
+    const _concert_this = function(value) {
         const queryUrl = "https://rest.bandsintown.com/artists/" + encodeURIComponent(value.trim()) 
             + "/events?app_id=codingbootcamp";
         _axios.get(queryUrl)
         .then(function(response) {
-            response.data.forEach((concert) => {
-                _output("===========================================================");
-                _output("Venue name: " + concert.venue.name);
-                _output("Location: " + concert.venue.city + ", " + concert.venue.country);
-                const moment = require("moment");
-                _output("Date: " + moment(concert.datetime).format("MMMM Do YYYY, h:mm:ssa"));
-                _output("");
-            });
-        });
-    }
-    
-    const _spotify_this = function(value="Ace of Base - The Sign") {
-        _spotify.search({ type: "track", query: encodeURIComponent(value.trim())}, function(error, data) {
-            if (error) {
-                return _output("Error occurred: " + error);
+            if (Array.isArray(response.data)) {
+                response.data.forEach((concert) => {
+                    _output("", "===========================================================");
+                    _output("Venue name:", concert.venue.name);
+                    _output("Location:", concert.venue.city + ", " + concert.venue.country);
+                    const moment = require("moment");
+                    _output("Date:", moment(concert.datetime).format("MMMM Do YYYY, h:mm:ssa"));
+                    _output("", "");
+                });
+            } else {
+                _output("", "Artist not found.", true, true);
             }
-            const item = data.tracks.items[0];
-            const artists = item.artists.map((artist) => artist.name).join(", ");
-            _output("Name: " + item.name);
-            _output("Artists: " + artists);
-            _output("Album: " + item.album.name);
-            _output("Spotify link: " + item.external_urls.spotify);
+            _outputEnd();
         });
     }
     
-    const _movie = function(value="Mr. Nobody") {
+    const _do_what_it_says = function() {
+        _fs.readFile("random.txt", "utf8", function(error, input) {
+            if (error) return console.log(error);
+            const [command, value] = input.split(",");
+            _commandSwitch(command, value);
+        });
+    }
+
+    const _init = function(command, value) {
+        _output("", "***********************************************************", false);
+        _output("", command + " " + (value != void 0 ? value : ""), false);
+        _commandSwitch(command, value);
+    }
+    
+    const _movie_this = function(value="Mr. Nobody") {
         const queryUrl = "http://www.omdbapi.com/?t=" + encodeURIComponent(value.trim()) + "&apikey=trilogy";
         _axios.get(queryUrl)
             .then(function(response) {
                 const movie = response.data;
-                _output("Title: " + movie.Title);
-                _output("Year: " + movie.Year);
-                _output("IMDB rating: "+ movie.imdbRating);
-                const rottenTomatoes = movie.Ratings.find((rating) => {
-                    if (rating.Source === "Rotten Tomatoes") return rating;
-                });
-                _output("RottenTomatoes rating: " + rottenTomatoes.Value);
-                _output("Country: " + movie.Country);
-                _output("Language: " + movie.Language);
-                _output("Plot: " + movie.Plot);
-                _output("Actors: " + movie.Actors);
+                if (movie.Title !== void 0) {
+                    _output("Title:", movie.Title);
+                    _output("Year:", movie.Year);
+                    _output("IMDB rating:", movie.imdbRating);
+                    const rottenTomatoes = movie.Ratings.find((rating) => {
+                        if (rating.Source === "Rotten Tomatoes") return rating;
+                    });
+                    _output("RottenTomatoes rating:", rottenTomatoes.Value);
+                    _output("Country:", movie.Country);
+                    _output("Language:", movie.Language);
+                    _output("Plot:", movie.Plot);
+                    _output("Actors:", movie.Actors);
+                } else {
+                    _output("", "Movie not found.", true, true);
+                }
+                _outputEnd();
             });
     }
-    
-    const _random = function() {
-        _output("random");
-        _commandSwitch("concert-this", "I Want it That Way");
-    }
-    
-    
-    const _commandSwitch = function(command, value) {
-        _output("***********************************************************");
-        _output(command + " " + (value != void 0 ? value : ""));
-        switch (command) {
-            case "concert-this":
-                _concert(value);
-                break;
-            case "spotify-this-song":
-                _spotify_this(value);
-                break;
-            case "movie-this":
-                _movie(value);
-                break;
-            case "do-what-it-says":
-                _random();
-                break;
-            default:
-                _output("Please input a valid command.");
-                break;
+
+    const _output = function(title, message, showInTerminal=true, isError=false) {
+        // do this better
+        if (showInTerminal) {
+            if (title.length > 0) {
+                console.log("\x1b[32m", title, "\x1b[36m", " " + message, "\x1b[0m");
+            } else {
+                if (isError) {
+                    console.log("\x1b[31m", "ERROR: " + message, "\x1b[0m");
+                } else {
+                    console.log(message);
+                }
+            }
         }
-        _output("***********************************************************");
-        _output("");
+        // do this so they don't get out of order
+        _fs.appendFile("log.txt", message + "\n", function(error) {
+            if (error) return console.log(error);
+        });
     }
-    _commandSwitch(command, value);
+
+    const _outputEnd = function() {
+        _output("", "***********************************************************", false);
+        _output("", "", false);
+    }
+    
+    const _spotify_this_song = function(value="Ace of Base - The Sign") {
+        _spotify.search({ type: "track", query: value}, function(error, data) {
+            if (error) return _output("Error occurred: " + error);
+            const item = data.tracks.items[0];
+            if (item !== void 0) {
+                const artists = item.artists.map((artist) => artist.name).join(", ");
+                _output("Name:", item.name);
+                _output("Artists:", artists);
+                _output("Album:", item.album.name);
+                _output("Spotify link:", item.external_urls.spotify);
+            } else {
+                _output("", "Song not found.", true, true);
+            }
+            _outputEnd();
+        });
+    }
+
+    _init(command, value);
 })(command, value);
