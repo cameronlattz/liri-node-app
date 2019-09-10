58d707b02da34230b3920bca825a2ba8d744f248
Cameron Lattz <camerolattz@gmail.com>
2019-09-10 13:34:31 -0500

    adding spotify functionality

--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const keys = require("./keys.js");
 const [,,command,...args] = process.argv;
-const value = args.join(" ");
+const value = args.length > 0 ? args.join(" ") : void 0;
 
 const liri = (function(command, value) {
     const _output = function(message) {
@@ -12,10 +12,21 @@ const liri = (function(command, value) {
         _output("concert " + value);
     }
     
-    const _spotify = function(value) {
+    const _spotify = function(value="Ace of Base - The Sign") {
         _output("spotify " + value);
         const Spotify = require("node-spotify-api");
         var spotify = new Spotify(keys.spotify);
+        spotify.search({ type: "track", query: value}, function(error, data) {
+            if (error) {
+                return _output("Error occurred: " + error);
+            }
+            const item = data.tracks.items[0];
+            const artists = item.artists.map((artist) => artist.name).join(", ");
+            _output("Name: " + item.name);
+            _output("Artists: " + artists);
+            _output("Album: " + item.album.name);
+            _output("Spotify link: " + item.external_urls.spotify);
+        });
     }
     
     const _movie = function(value) {
